aab539e9edc2b0fd683bf0925360ce3ec4dd8be0
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-06-15 20:58:21 -0300

    benchmark koa app
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

---
+++ koa-hello-world/README.md
@@ -0,0 +1,17 @@
+## Benchmarking a NodeJS Server
+
+Using the `0x` and `autocannon` packages it's possible to create flamegraphs on your server's load test.
+
+To create one yourself just run:
+
+```
+yarn start
+```
+
+Then:
+
+```
+yarn bench
+```
+
+Then interrupt the server with Ctrl+C.
---
+++ koa-hello-world/app.js
@@ -0,0 +1,25 @@
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+/* app.js; minimal 'hello world' app (using router & handlebars)                                  */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+
+const Koa        = require('koa');            // koa framework
+const router     = require('koa-router')();   // router middleware for koa
+const handlebars = require('koa-handlebars'); // handlebars templating
+
+const app = new Koa();
+
+app.use(handlebars({
+    extension: [ 'html' ],
+    viewsDir:  '.'
+}));
+
+router.get('/', async function(ctx, next) {
+    const context = { version: process.version, time: new Date() };
+    console.log('router.get /', context);
+    await ctx.render('hello-world', context);
+});
+
+app.use(router.routes());
+
+app.listen(3001);
+console.log('listening on 3001');
